88c8535e73639b7bc36831a9e50d6f28cedbabfb
Christopher Høe <christopher.hoee@gmail.com>
2025-02-23 16:04:15 +0100

    Fixed .gitignore and tracked lab folders

---
+++ lab-l2-handout/user/run_loads.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+echo "Run #1"
+time sh load.sh
+echo
+
+echo "Run #2"
+time sh load.sh
+echo
+
+echo "Run #3"
+time sh load.sh
+echo
+
+echo "Run #4"
+time sh load.sh
+echo
+
+echo "Run #5"
+time sh load.sh
+echo
+
+echo "Run #6"
+time sh load.sh
+echo
+
+echo "Run #7"
+time sh load.sh
+echo
+
+echo "Run #8"
+time sh load.sh
+echo
+
+echo "Run #9"
+time sh load.sh
+echo
+
+echo "Run #10"
+time sh load.sh
+echo
